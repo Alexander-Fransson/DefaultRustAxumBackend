28a449f02cf1676967ec597c9656f7c54a51323e
Alexander-Fransson <festusOp7@gmail.com>
2025-02-25 20:46:43 +0100

    [feat] created the custom extractor for the request context

--- backend/src/gate/custom_extractors/request_context_extractor.rs
+++ backend/src/gate/custom_extractors/request_context_extractor.rs
@@ -1,15 +1,12 @@
 use axum::extract::FromRequestParts;
 use axum::http::request::Parts;
-use async_trait::async_trait;
 
 use crate::request_context::RequestContext;
-
 use super::{Error, Result};
 
-
-
-#[async_trait]
-impl <S: Send + Sync> FromRequestParts<S> for RequestContext {
+// async trait leads to an error here, and it compiles without it so maybe it is not needed
+impl <S> FromRequestParts<S> for RequestContext where S: Send + Sync {
+    // this does not extract from the body
     type Rejection = Error;
 
     async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
@@ -21,4 +18,3 @@ impl <S: Send + Sync> FromRequestParts<S> for RequestContext {
     }
 }
 
-// need to rethink error handling on this floor maybe have one master and many lesser
---
+++ backend/src/gate/middlewares/mod.rs
@@ -0,0 +1,1 @@
+pub mod mw_extract_request_context;
